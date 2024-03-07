73a2ef130ceb5e738d10be6b9d0a7140683a14cb
MoWael45 <mowael450@gmail.com>
2024-03-07 15:53:57 +0200

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Importing requests module
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    returns the number of subscribers
+    If not a valid subreddit, return 0.
+    """
+    # Check if the subredit is none, Return 0
+    if subreddit is None or subreddit is not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    # Identifiying the user agent
+    userAgent = {' User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    # Identify the url
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    # Make the requesst
+    response = get(url, head=userAgent)
+
+    # Convert the response intp json format
+    jsonResponse = response.json()
+
+    # Try to get the number of subscribes
+    try:
+        return jsonResponse.get('data').get('subscribers')
+    # If you could't get it
+    except exceptions.RequestException as e:
+        return 0
